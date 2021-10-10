0f0eb0b46bc645224b1df8530d1114dbed7ddf0a
HOME <coderiding9060@gmail.com>
2021-10-10 23:11:33 +0900

    Initial commit

---
+++ src/main/java/com/cnw/diarygram/DiarygramApplication.java
@@ -0,0 +1,13 @@
+package com.cnw.diarygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarygramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarygramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnw/diarygram/DiarygramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnw.diarygram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarygramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
